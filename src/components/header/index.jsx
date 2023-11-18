c54436599e308f793151070a456758c3a0efa4c4
fomes <fgomesdeluna@gmail.com>
2023-11-18 11:11:11 -0300

    Redux config, userReducer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
-
-// Components
 import Cart from "../cart/index";
-
-// Styles
 import * as Styles from "./styles";
+
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import UserActionTypes from "../../redux/user/action-types";
 
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
@@ -13,11 +12,34 @@ function Header() {
     setCartIsVisible(true);
   };
 
+  const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
+
+  const dispatch = useDispatch();
+
+  const handleLoginClick = () => {
+    dispatch({
+      type: UserActionTypes.LOGIN,
+      payload: { name: "Felipe", email: "felipe@mail.com" },
+    });
+  };
+
+  const handleLogoutClick = () => {
+    dispatch({
+      type: UserActionTypes.LOGOUT,
+    });
+  };
+
+  console.log({ currentUser });
+
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
+        {currentUser ? (
+          <div onClick={handleLogoutClick}>Sair</div>
+        ) : (
+          <div onClick={handleLoginClick}>Login</div>
+        )}
         <div onClick={handleCartClick}>Carrinho</div>
       </Styles.Buttons>
 
